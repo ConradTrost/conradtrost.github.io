636cd21315b0de7f8a12f66a763d06cc59cd265b
Conrad Trost <ctrost@tamu.edu>
2020-05-03 15:44:01 -0500

    uodate navbar and added hero on about

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,16 @@
 var mainSection = $('.section-holder');
+var heroSection = $('.hero');
 
 function aboutFunction() {
     $('.orange').removeClass('orange');
     $('#about').addClass('orange');
     mainSection.html('');
+    heroSection.html('');
+    var hero = '<div class="images container-fluid"><img src="assets/images/cami.jpg" alt="Camila" id="cami"><img src="assets/images/koda.jpg" alt="Koda" id="koda"></div>'
     var about = '<p>Howdy! I am a web developer originating from South Florida but currently residing in Dallas, TX. </p>'
     + '<p>I have a wonderful wife, Camila, and a gorgeous German Shepherd named Koda.</p>'
-    + '<div class="images"><img src="assets/images/cami.jpg" alt="Camila" id="cami"><img src="assets/images/koda.jpg" alt="Koda" id="koda"></div>'
     + '<p>My hobbies consist of playing guitar, teaching my dog how to play guitar, and giving up on my dream of teaching my dog to play guitar.</p>';
+    heroSection.append(hero);
     mainSection.append(about);
 }
 
@@ -15,6 +18,7 @@ function skillsFunction() {
     $('.orange').removeClass('orange');
     $('#skills').addClass('orange');
     mainSection.html('');
+    heroSection.html('');
     var skills = '<div class="container-fluid skillset"><div class="skill-icons"><div class="icon-contain"><i class="col fab fa-html5"></i>HTML</div>'
     + '<div class="icon-contain"><i class="col fab fa-css3-alt"></i>CSS</div>'
     + '<div class="icon-contain"><i class="col fab fa-bootstrap"></i>Bootstrap</div>'
@@ -29,6 +33,7 @@ function projectFunction() {
     $('#projects').addClass('orange');
     $(this).addClass('orange');
     mainSection.html('');
+    heroSection.html('');
     var projects = '<p>I have a couple projects</p>';
     mainSection.append(projects);
 }
@@ -37,6 +42,7 @@ function educationFunction() {
     $('.orange').removeClass('orange');
     $('#edu').addClass('orange');
     mainSection.html('');
+    heroSection.html('');
     var education = '<p>I am a current Aggie in my junior year at Texas A&M University studying Computer Engineering.</p>'
     + "<p>I'm also currently a student at UT Austin Coding Bootcamp.</p>"
     + '<p>My cumulative GPA is 3.6 and I am dropping out.</p>';
