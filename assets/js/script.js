6afbde468b51a391ebedc196bc5775c702455cc0
Conrad Trost <ctrost@tamu.edu>
2020-05-03 14:11:39 -0500

    added skills and images

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,7 @@ function aboutFunction() {
     mainSection.html('');
     var about = '<p>Howdy! I am a web developer originating from South Florida but currently residing in Dallas, TX. </p>'
     + '<p>I have a wonderful wife, Camila, and a gorgeous German Shepherd named Koda.</p>'
+    + '<div class="images"><img src="assets/images/cami.jpg" alt="Camila" id="cami"><img src="assets/images/koda.jpg" alt="Koda" id="koda"></div>'
     + '<p>My hobbies consist of playing guitar, teaching my dog how to play guitar, and giving up on my dream of teaching my dog to play guitar.</p>';
     mainSection.append(about);
 }
@@ -14,7 +15,12 @@ function skillsFunction() {
     $('.orange').removeClass('orange');
     $('#skills').addClass('orange');
     mainSection.html('');
-    var skills = '<p>I am from New York</p>';
+    var skills = '<div class="container-fluid skillset"><div class="skill-icons"><div class="icon-contain"><i class="col fab fa-html5"></i>HTML</div>'
+    + '<div class="icon-contain"><i class="col fab fa-css3-alt"></i>CSS</div>'
+    + '<div class="icon-contain"><i class="col fab fa-bootstrap"></i>Bootstrap</div>'
+    + '<div class="icon-contain"><i class="col fab fa-js"></i>Javascript</div>'
+    + '<div class="icon-contain"><i class="col fab fa-git-square"></i>Git</div>'
+    + '<div class="icon-contain"><i class="col fab fa-python"></i>Python</div></div>';
     mainSection.append(skills);
 }
 
@@ -31,7 +37,9 @@ function educationFunction() {
     $('.orange').removeClass('orange');
     $('#edu').addClass('orange');
     mainSection.html('');
-    var education = '<p>I am smart</p>';
+    var education = '<p>I am a current Aggie in my junior year at Texas A&M University studying Computer Engineering.</p>'
+    + "<p>I'm also currently a student at UT Austin Coding Bootcamp.</p>"
+    + '<p>My cumulative GPA is 3.6 and I am dropping out.</p>';
     mainSection.append(education);
 }
 
