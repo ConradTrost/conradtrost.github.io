dee28c7388f3e2eb08c2f64f4d9eb06c10abe846
Conrad Trost <ctrost@tamu.edu>
2020-05-18 14:22:50 -0500

    Remove education section and filled about

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,9 +9,18 @@ function aboutFunction() {
     $('#main-btn').text('About');
     var hero = '<div class="images container-fluid"><img src="assets/images/cami.jpg" alt="Camila" id="cami"><img src="assets/images/koda.jpg" alt="Koda" id="koda"></div>'
     var about = 
-    `<p>Howdy! I am a web developer originating from South Florida but currently residing in Dallas, TX. </p>
-    <p>I have a wonderful girlfriend, Camila, and a gorgeous German Shepherd named Koda.</p>
-    <p>My hobbies consist of playing guitar, teaching my dog how to play guitar, and giving up on my dream of teaching my dog to play guitar.</p>`;
+    `<div class="container">
+        <p class="aboutLeft">Howdy! I am a full stack web developer proudly from Dallas, Texas.<br></br>
+        I have a wonderful girlfriend, Camila, and a gorgeous German Shepherd named Koda. I am a current third-year student at Texas A&M University studying Computer Engineering.<br></br>
+        I am a tinkerer. I love to mess around with electronics and software as a way of understanding their complex structures and learning what I can create with it.<br></br>
+        I have a desire to learn. When I was 8 I began playing guitar and I have yet to stop the learning journey. I believe learning is never meant to end and that there is always more knowledge and ability to acquire.</p>
+        <img src="assets/images/cami.jpg" alt="Camila" id="cami"><img src="assets/images/koda.jpg" alt="Koda" id="koda">
+        <p class="aboutRight">I am a self starter. My interest in web development began in 2017 during the "Shopify Boom". I created 3 websites using Shopify's templates but found that to not give me enough flexibility in my designs.
+         The main things I learned during these side projects were the importance of <span class="orange-t">SEO</span> and <span class="orange-t">branding</span>.<br></br>
+        I began messing around with HTML and CSS but did not take it seriously until my second year at college. I learned Python, and more importantly, learned that Javascript was the language I wanted to master for front-end and back-end development.<br></br>
+        After my second year, I decided web development is what I want to pursue, and I enrolled into <span class="orange-t">UT Austin Coding Bootcamp</span> to develop my skills and get into the field I am passionate about.<br></br>
+        I am competitive. I continued to work on my own projects to master the concepts and languages taught in the bootcamp and take more control of the potential in software.
+    </div>`;
     // heroSection.append(hero);
     mainSection.append(about);
 }
@@ -35,8 +44,8 @@ function skillsFunction() {
         </div>
         <div class="skills-text">
             <p>Full stack web developer specialized in <span class="orange-t">Javascript</span> and <span class="orange-t">Node.js</span></p>
-            <p>Experienced front-end designer with primary utilization of Bootstrap and CSS animations.</p>
-            <p>Back-end architect with an established understanding of Node.js and server-side API's. My main focus is quick rendering for speedy loading times and minimal data usage.</p>
+            <p>Experienced front-end designer. I use <span class="orange-t">Figma</span> to create prototypes before mocking the design in CSS and HTML. I have experience using <span class="orange-t">Bootstrap</span>, although I prefer minimal framework usage.</p>
+            <p>Back-end architect with an established understanding of <span class="orange-t">Node.js</span> and server-side API's. My main focus is quick loading times and minimal data usage.</p>
             <p>My main skill trait is my ability to learn quickly, efficiently, and well. I have always been a self-learner and I am proud to say that I have a developed ability to grasp new concepts and retain new information.</p>
         </div>
     </div>`;
@@ -59,17 +68,39 @@ function projectFunction() {
     mainSection.append(projects);
 }
 
-function educationFunction() {
+// function educationFunction() {
+//     $('.orange').removeClass('orange');
+//     $('#edu').addClass('orange');
+//     $('#main-btn').text('Education')
+//     mainSection.html('');
+//     heroSection.html('');
+//     var education = 
+//     `<div class="container">
+//         <p class="left rel">Current student at UT Austin Coding Bootcamp studying web development.</p>
+//         <p class="right rel">Current Aggie at Texas A&M University studying Computer Engineering.</p>
+//         <p class="rel full">Throughout my education I have always prioritized self-learning. I believe this was an important step in developing my skills to the <span class="orange-t">full</span> potential.</p>
+//     </div>`;
+//     mainSection.append(education);
+// }
+
+function contactFunction() {
     $('.orange').removeClass('orange');
-    $('#edu').addClass('orange');
+    $('#contact').addClass('orange');
     $('#main-btn').text('Education')
     mainSection.html('');
     heroSection.html('');
-    var education = 
-    `<p class="left rel">Current student at UT Austin Coding Bootcamp studying web development.</p>
-    <p class="right rel">Current Aggie at Texas A&M University studying Computer Engineering.</p>
-    <p class="rel full">Throughout my education I have always prioritized self-learning. I believe this was an important step in developing my skills to the <span class="orange-t">full</span> potential.</p>`;
-    mainSection.append(education);
+    var contactSection = `
+    <div class="container">
+        <h1>Shoot me an email!</h1>
+        <p>I'd be happy to respond.</p>
+    </div>
+    `;
+    mainSection.append(contactSection);
 }
 
 aboutFunction();
+
+// Remove heroSection??
+// Remove project transparent thing and change it to hover w/ languages declared.
+// Make about section mobile responsive please.
+// Contact section using Node.js
