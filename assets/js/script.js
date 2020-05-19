dc1ea6dce79627d7d3c0387858b8b640f8a838db
Conrad Trost <ctrost@tamu.edu>
2020-05-19 14:13:37 -0500

    Changed project section design

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,16 +10,16 @@ function aboutFunction() {
     var hero = '<div class="images container-fluid"><img src="assets/images/cami.jpg" alt="Camila" id="cami"><img src="assets/images/koda.jpg" alt="Koda" id="koda"></div>'
     var about = 
     `<div class="container aboutContain" id="about-id">
-        <p class="aboutLeft">Howdy! I am a full stack web developer proudly from Dallas, Texas.<br></br>
-        I have a wonderful girlfriend, Camila, and a gorgeous German Shepherd named Koda. I am a current third-year student at Texas A&M University studying Computer Engineering.<br></br>
+        <p class="aboutLeft">Howdy! I am a <span class="orange-t">Full-Stack Web Developer</span> proudly from Dallas, Texas.<br></br>
+        I have a wonderful girlfriend, Camila, and a gorgeous German Shepherd named Koda. I am a current third-year student at <span class="orange-t">Texas A&M University</span> studying Computer Engineering.<br></br>
         I am a tinkerer. I love to mess around with electronics and software as a way of understanding their complex structures and learning what I can create with it.<br></br>
         I have a desire to learn. When I was 8 I began playing guitar and I have yet to stop the learning journey. I believe learning is never meant to end and that there is always more knowledge and ability to acquire.</p>
         <img src="assets/images/cami.jpg" alt="Camila" id="cami"><img src="assets/images/koda.jpg" alt="Koda" id="koda">
-        <p class="aboutRight">I am a self starter. My interest in web development began in 2017 during the "Shopify Boom". I created 3 websites using Shopify's templates but found that to not give me enough flexibility in my designs.
+        <p class="aboutRight">I am a self-starter. My interest in web development began in 2017 during the "Shopify Boom". I created 3 websites using Shopify's templates but found that to not give me enough flexibility in my designs.
          The main things I learned during these side projects were the importance of <span class="orange-t">SEO</span> and <span class="orange-t">branding</span>.<br></br>
         I began messing around with HTML and CSS but did not take it seriously until my second year at college. I learned Python, and more importantly, learned that Javascript was the language I wanted to master for front-end and back-end development.<br></br>
-        After my second year, I decided web development is what I want to pursue, and I enrolled into <span class="orange-t">UT Austin Coding Bootcamp</span> to develop my skills and get into the field I am passionate about.<br></br>
-        I am competitive. I continued to work on my own projects to master the concepts and languages taught in the bootcamp and take more control of the potential in software.
+        After my second year, I decided web development is what I want to pursue, and I enrolled in <span class="orange-t">UT Austin Coding Bootcamp</span> to develop my skills and get into the field I am passionate about.<br></br>
+        I am competitive. I continued to work on my projects to master the concepts and languages taught in the Bootcamp and take more control of the potential in software.
     </div>`;
     // heroSection.append(hero);
     mainSection.append(about);
@@ -43,9 +43,9 @@ function skillsFunction() {
             </div>
         </div>
         <div class="skills-text">
-            <p>Full stack web developer specialized in <span class="orange-t">Javascript</span> and <span class="orange-t">Node.js</span></p>
+            <p>Full-stack web developer specialized in <span class="orange-t">Javascript</span> and <span class="orange-t">Node.js</span></p>
             <p>Experienced front-end designer. I use <span class="orange-t">Figma</span> to create prototypes before mocking the design in CSS and HTML. I have experience using <span class="orange-t">Bootstrap</span>, although I prefer minimal framework usage.</p>
-            <p>Back-end architect with an established understanding of <span class="orange-t">Node.js</span> and server-side API's. My main focus is quick loading times and minimal data usage.</p>
+            <p>Back-end architect with an established understanding of <span class="orange-t">Node.js</span> and server-side APIs. My main focus is quick loading times and minimal data usage.</p>
             <p>My main skill trait is my ability to learn quickly, efficiently, and well. I have always been a self-learner and I am proud to say that I have a developed ability to grasp new concepts and retain new information.</p>
         </div>
     </div>`;
@@ -60,10 +60,35 @@ function projectFunction() {
     mainSection.html('');
     heroSection.html('');
     var projects = 
-    `<div class="containers row" id="project-id">
-        <div id="neoo" class="proj module"><header><h1><a href="https://retro1967.github.io/NEOO">NEOO</a></div>
-        <div id="weather-dashboard" class="proj module"><header><h1><a href="https://retro1967.github.io/weather-dashboard">Weather Dashboard</a></div>
-        <div id="run-buddy" class="proj module"><header><h1><a href="https://retro1967.github.io/run-buddy">Run Buddy</a></div>
+    `
+    <h2 class="rem-a">Head to my <a href="https://github.com/retro1967">Github</a> to see everything I've been working on!</h2>
+    <div class="containers row" id="project-id">
+        <div class="proj-container proj">
+            <a href="https://retro1967.github.io/NEOO">
+                <img class="image" src="./assets/images/nasa.png">
+                <div class="middle"><h1>NEOO</h1>
+                    <p>Third-party APIs and Foundation CSS framework</p>
+                </div>
+            </a>
+        </div>
+
+        <div class="proj-container proj">
+            <a href="https://retro1967.github.io/weather-dashboard">
+                <img class="image" src="./assets/images/weather.png">
+                <div class="middle"><h1>Weather Dashboard</h1>
+                    <p>OpenWeatherMap API</p>
+                </div>
+            </a>
+        </div>
+
+        <div class="proj-container proj">
+            <a href="https://retro1967.github.io/run-buddy">
+                <img class="image" src="./assets/images/run.png">
+                <div class="middle"><h1>Run Buddy</h1>
+                    <p>Static HTML and CSS</p>
+                </div>
+            </a>
+        </div>
     </div>`;
     mainSection.append(projects);
 }
@@ -104,7 +129,7 @@ function contactFunction() {
     mainSection.append(contactSection);
 }
 
-aboutFunction();
+projectFunction();
 
 // Remove heroSection??
 // Remove project transparent thing and change it to hover w/ languages declared.
