90ef491961c193cfbeb5ebfbde64ec42e9e72228
Conrad Trost <ctrost@tamu.edu>
2020-12-11 15:41:57 -0600

    Start of portfolio rework

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,13 +8,13 @@ export class Footer extends Component {
                     <a href="https://github.com/retro1967" 
                         rel="noopener noreferrer" 
                         target="_blank">
-                        <i className="fab fa-github"></i>
+                        <i className="fab fa-github footer-btn"></i>
                     </a>
 
                     <a href="https://www.linkedin.com/in/conrad-trost-635b591a0/" 
                         rel="noopener noreferrer" 
                         target="_blank">
-                        <i className="fab fa-linkedin-in"></i>
+                        <i className="fab fa-linkedin-in footer-btn"></i>
                     </a>
 
                 </div>
