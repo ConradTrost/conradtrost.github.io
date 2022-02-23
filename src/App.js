da8f7394801f51dccd7ff26044b62563d0a54279
conradtrost <ct.conradtrost@gmail.com>
2022-02-23 01:19:00 -0600

    patch routing issuse

--- src/App.js
+++ src/App.js
@@ -1,35 +1,40 @@
 import React from "react";
 
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Loading from "./components/loading.js";
-import AboutPage from './pages/AboutPage';
-import ProjectPage from './pages/ProjectPage';
-import ContactPage from './pages/ContactPage';
-import ResumePage from './pages/ResumePage';
+import AboutPage from "./pages/AboutPage";
+import ProjectPage from "./pages/ProjectPage";
+import ContactPage from "./pages/ContactPage";
+import ResumePage from "./pages/ResumePage";
 
 import "./App.scss";
 
 // analytics
-import ReactGA from 'react-ga';
+import ReactGA from "react-ga";
 const TRACKING_ID = "UA-188054952-1";
 ReactGA.initialize(TRACKING_ID);
 
-ReactDOM.render(<Router basename={process.env.PUBLIC_URL}>< App /></Router>, document.getElementById('root'));
+ReactDOM.render(
+  <Router basename={process.env.PUBLIC_URL}>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
 
 function App() {
   return (
-    // <Router>
-      <div className="App">
-          <Route path="/" exact render={() => <Loading />} />
-          <Route path="/about" render={() => <AboutPage />} />
-          <Route path="/projects" render={() => <ProjectPage />} />
-          <Route path="/contact" render={() => <ContactPage />} />
-          <Route path="/resume" render={() => <ResumePage />} />
-      </div>
-    // </Router>
+    <div className="App">
+      <Routes>
+        <Route path="/" exact element={<Loading />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/projects" element={<ProjectPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/resume" element={<ResumePage />} />
+      </Routes>
+    </div>
   );
 }
 
-export default App; 
+export default App;
